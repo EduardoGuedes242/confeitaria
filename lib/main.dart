8978a15a6da996ed3a347903adbad2f7758ad1e7
Eduardo Guedes <eduardoguedesdev@gmail.com>
2024-10-10 13:26:46 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:confeitaria/pages/home_page.dart';
-import 'package:confeitaria/ui/cores.dart';
-import 'package:confeitaria/ui/input.dart';
-import 'package:confeitaria/ui/text.dart';
-import 'package:confeitaria/ui/widgets/button.dart';
-import 'package:confeitaria/ui/widgets/combobox.dart';
-import 'package:confeitaria/ui/widgets/edit.dart';
-import 'package:confeitaria/ui/widgets/item_menu.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,13 +10,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Confeitaria',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
